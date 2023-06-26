af3256ca08e67abf388fa67cfdab07ce80d2f682
José Cardoso <caardoso4@outlook.com>
2023-06-25 21:08:37 -0300

    modified

---
+++ script.js
@@ -0,0 +1,23 @@
+const pokemon = [
+    {
+        id: "1",
+        name: "Bicho",
+        img: "https://res.cloudinary.com/dmm3wcupr/image/upload/v1687735050/pokemon/pokemons/001_ps3twt.png"
+    },
+    {
+        id: "2",
+        name: "Bichano",
+        img: "https://res.cloudinary.com/dmm3wcupr/image/upload/v1687735050/pokemon/pokemons/002_aux90m.png"
+    }
+];
+
+const prevBtn = document.getElementsByClassName("prev")[0];
+const nextBtn = document.getElementsByClassName("next")[0];
+
+prevBtn.addEventListener("click", function(){
+    
+});
+
+nextBtn.addEventListener("click", function(){
+    
+})
